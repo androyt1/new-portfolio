8f4e4e36f3dc562cfd46f06077a67a71ea780eb6
Andrew Aghoghovwia <androyt1@gmail.com>
2024-07-14 08:44:51 +0100

    adjusted space

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className='w-full  h-20 sticky top-0 left-0 z-20  bg-gray-900 text-white flex items-center px-5'>
             <div className='max-w-[1280px] mx-auto flex justify-between items-center flex-1'>
-                <div className=' flex items-center space-x-4'>
+                <div className=' flex items-center space-x-2'>
                     <span className='text-2xl font-semibold'>Androy</span>
                     <NameSvg />
                 </div>
