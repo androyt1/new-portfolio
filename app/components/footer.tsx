5b9b7716ad41e228546cbfc2de9872d83162fda3
Andrew Aghoghovwia <androyt1@gmail.com>
2024-07-15 08:35:22 +0100

    Added footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className='h-screen w-full flex flex-col justify-center items-center bg-black text-white'>
+            <span>Footer</span>
+        </footer>
+    );
+}
+
+export default Footer;
